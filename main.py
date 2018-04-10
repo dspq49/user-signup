9b711edcacb67465b7f70d96de17b85ba24c1019
Danielle Patton <dsashapatton@gmail.com>
2018-04-09 19:58:37 -0500

    Updated

--- main.py
+++ main.py
@@ -1,63 +1,78 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+import jinja2
 
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-<html>
-    <head>
-        <style>
-            .error { color: red;
-                }
-        </style>
-    </head>
-    <body>
-        <h1>Signup</h1>
-        <form method="post">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <label for="username">Username</label
-                        </td>
-                        <td>
-                            <input name="username" value="" type="text">
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password">Password</label>
-                        </td>
-                        <td>
-                            <input name="password" type="password">
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="verify">Verify Password</label>
-                        </td>
-                        <td>
-                            <input name="verify" type="password">
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email"Email (optional)</label>
-                        </td>
-                        <td>
-                            <input name="email" value="">
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <input type="submit">
-        </form>
-    </body>
-</html>
-"""
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('base.html')
+    return template.render()
+
+
+
+
+@app.route('/validate-signup', methods=['POST'])
+def valid_signup():
+
+    username = request.form['username']
+    password_input = request.form['password_input']
+    password_verify = request.form['password_verify']
+    email = request.form['email']
+
+    
+    password_error = ''
+    username_error = ''
+    password_verify_error = ''
+    email_error = ''
+
+    if username == '':
+        username_error = 'Enter Username'
+
+    else:
+         if len(username) < 3 or len(username) > 20:
+            username_error = 'Invaild username'
+
+
+    if password_input == '':
+        password_error = 'Enter password'
+
+    else:
+        
+         if len(password_input) < 3 or len(password_input) > 20:
+            password_error = 'Invaild password'
+            
+
+    if password_input != password_verify:
+        password_verify_error = 'Please make passwords match'
+    
+
+    if email == '':
+        email = email
+    else:
+        if len(email) < 3 or len(email) > 20:
+         email_error = 'invaild email'
+    
+    if not username_error and not password_error:
+        username = username
+        return redirect('/vaild-signup?username={0}'.format(username))
+    else:
+        template = jinja_env.get_template('base.html')
+        return template.render( username=username, password_error=password_error, username_error=username_error, password_input=password_input, password_verify_error=password_verify_error,email_error=email_error) 
+        
+
+
+@app.route('/vaild-signup')
+def vaild_signup():
+    username = request.args.get('username')
+    return render_template('enter.html', username=username)
+
+app.run()
+
